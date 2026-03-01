35489e1431a0d05eed750da0b55d4719f6e9dfc0
Pablo Alessandro Santos Hugen <phugen@redhat.com>
2026-03-01 00:27:51 -0300

    update
    Signed-off-by: Pablo Alessandro Santos Hugen <phugen@redhat.com>

--- build.zig
+++ build.zig
@@ -1,68 +1,77 @@
 const std = @import("std");
+const LinkMode = std.builtin.LinkMode;
+
 const manifest = @import("build.zig.zon");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const linkage = b.option(std.builtin.LinkMode, "linkage", "Library linkage type") orelse .static;
-
-    const upstream = b.dependency("upstream", .{});
-    const src = upstream.path("");
-
     const os = target.result.os.tag;
-    const is_linux = os == .linux;
+    const arch = target.result.cpu.arch;
 
-    const gen_fourcc = b.addExecutable(.{
-        .name = "gen_table_fourcc",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("tools/gen_table_fourcc.zig"),
-            .target = b.graph.host,
-        }),
-    });
-    const gen_run = b.addRunArtifact(gen_fourcc);
-    gen_run.addFileArg(src.path(b, "include/drm/drm_fourcc.h"));
-    const fourcc_wf = b.addWriteFiles();
-    _ = fourcc_wf.addCopyFile(gen_run.captureStdOut(.{}), "generated_static_table_fourcc.h");
-
-    // Module
-    const mod = b.createModule(.{ .target = target, .optimize = optimize, .link_libc = true });
-    mod.addIncludePath(src);
-    mod.addIncludePath(src.path(b, "include/drm"));
-    mod.addIncludePath(fourcc_wf.getDirectory());
+    const options = .{
+        .linkage = b.option(LinkMode, "linkage", "Library linkage type") orelse
+            .static,
+    };
 
     const flags: []const []const u8 = flags: {
-        var list: std.ArrayListUnmanaged([]const u8) = .empty;
-        try list.ensureTotalCapacity(b.allocator, 1 << 4);
+        var list: std.ArrayList([]const u8) = .empty;
+        try list.ensureTotalCapacity(b.allocator, 1 << 2);
 
-        list.appendSliceAssumeCapacity(&.{
-            "-fvisibility=hidden",
-            "-DHAVE_LIBDRM_ATOMIC_PRIMITIVES=1",
-            "-DHAVE_VISIBILITY=1",
-            "-D_GNU_SOURCE",
-            "-DHAVE_OPEN_MEMSTREAM=1",
-            "-DHAVE_SECURE_GETENV=1",
-            "-DHAVE_SYS_SELECT_H=1",
-            "-DHAVE_ALLOCA_H=1",
-        });
-        if (is_linux) list.appendAssumeCapacity("-DMAJOR_IN_SYSMACROS=1");
+        list.appendAssumeCapacity("-fvisibility=hidden");
+        if (os.isBSD()) list.appendSliceAssumeCapacity(&.{ "-Wno-macro-redefined", "-Wno-#warnings" });
+
         break :flags try list.toOwnedSlice(b.allocator);
     };
 
-    mod.addCSourceFiles(.{ .root = src, .files = &.{
-        "xf86drm.c",
-        "xf86drmHash.c",
-        "xf86drmRandom.c",
-        "xf86drmSL.c",
-        "xf86drmMode.c",
-    }, .flags = flags });
+    const upstream = b.dependency("libdrm_c", .{});
 
-    // Library
-    const lib = b.addLibrary(.{ .name = "drm", .root_module = mod, .linkage = linkage });
+    const fourcc_wf = b.addWriteFiles();
+    const gen = b.addRunArtifact(b.addExecutable(.{
+        .name = "gen_table_fourcc",
+        .root_module = b.createModule(.{ .root_source_file = b.path("tools/gen_table_fourcc.zig"), .target = b.graph.host }),
+    }));
+    gen.addFileArg(upstream.path("include/drm/drm_fourcc.h"));
+    _ = fourcc_wf.addCopyFile(gen.captureStdOut(.{}), "generated_static_table_fourcc.h");
 
-    // Install headers
+    const mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    mod.addIncludePath(upstream.path(""));
+    mod.addIncludePath(upstream.path("include/drm"));
+    mod.addIncludePath(fourcc_wf.getDirectory());
+    mod.addCMacro("HAVE_LIBDRM_ATOMIC_PRIMITIVES", "1");
+    mod.addCMacro("HAVE_VISIBILITY", "1");
+    mod.addCMacro("_GNU_SOURCE", "");
+    mod.addCMacro("HAVE_OPEN_MEMSTREAM", "1");
+    mod.addCMacro("HAVE_SECURE_GETENV", "1");
+    mod.addCMacro("HAVE_SYS_SELECT_H", "1");
+    mod.addCMacro("HAVE_ALLOCA_H", "1");
+    if (os == .linux) mod.addCMacro("MAJOR_IN_SYSMACROS", "1");
+    if (os.isBSD()) mod.addCMacro("HAVE_SYS_SYSCTL_H", "1");
+    if (arch.endian() == .big) mod.addCMacro("HAVE_BIG_ENDIAN", "1");
+
+    mod.addCSourceFiles(.{ .root = upstream.path(""), .files = srcs, .flags = flags });
+
+    const lib = b.addLibrary(.{
+        .name = "drm",
+        .root_module = mod,
+        .linkage = options.linkage,
+        .version = try .parse(manifest.version),
+    });
     inline for (.{ "xf86drm.h", "xf86drmMode.h", "libsync.h" }) |h|
-        lib.installHeader(src.path(b, h), h);
-    lib.installHeadersDirectory(src.path(b, "include/drm"), "libdrm", .{});
-
+        lib.installHeader(upstream.path(h), h);
+    lib.installHeadersDirectory(upstream.path("include/drm"), "libdrm", .{});
+    lib.installHeadersDirectory(upstream.path("include/drm"), "", .{});
     b.installArtifact(lib);
 }
+
+const srcs: []const []const u8 = &.{
+    "xf86drm.c",
+    "xf86drmHash.c",
+    "xf86drmRandom.c",
+    "xf86drmSL.c",
+    "xf86drmMode.c",
+};
